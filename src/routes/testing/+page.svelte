933d574c0dfe6e6e54de32b3f918f9ad29175457
Urban <josh@misterslandscaping.com>
2023-10-10 09:48:52 -0400

    To master

--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    import {shuffle} from 'lodash-es'
-    export let data: PageData;
-    let ff = [1,2,3,4,5,6,7,8,9,10];
-    let gg = shuffle(ff);
-    console.log(gg);
+	import type { PageData } from './$types';
+	import { shuffle } from 'lodash-es';
+	export let data: PageData;
+	let ff = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	let gg = shuffle(ff);
+	console.log(gg);
 </script>
